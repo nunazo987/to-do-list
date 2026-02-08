ce7a8f99b617527a903129c6a9e6386503f45d01
Nuno <nunomarquees@gmail.com>
2026-02-08 11:06:51 +0000

    update some text to english, remove button added, order of tasks after done is checked

--- script.js
+++ script.js
@@ -12,15 +12,43 @@ button.addEventListener('click', (e) => {
     const span = document.createElement('span');
     span.textContent = text;
 
-    span.addEventListener('click', () => {
-        span.classList.toggle('done');
-    });
-
     li.appendChild(span);
     taskList.appendChild(li);
+
+    span.addEventListener('click', () => {
+        span.classList.toggle('done');
+
+        if (span.classList.contains('done')){
+            if (!li.querySelector('.remove')){
+                const botaoRemover = document.createElement('button');
+                botaoRemover.textContent = 'x';
+                botaoRemover.className = 'remove';
+
+                botaoRemover.addEventListener('click', (e) => {
+                    e.stopPropagation();
+                    li.remove();
+                });
+                li.appendChild(botaoRemover);
+        }
+    } else {
+        const botao = li.querySelector('.remove');
+        if (btn) li.removeChild(btn);
+    }
+        reorderTasks();
+    });
 
     input.value = '';
 });
 
+function reorderTasks(){
+    const items = Array.from(taskList.children);
 
+    items.sort((a, b) => {
+        const aDone = a.querySelector('span').classList.contains('done');
+        const bDone = b.querySelector('span').classList.contains('done');
+        return aDone - bDone;
+
+    })
+    .forEach(li => taskList.appendChild(li));
+};
 
